23d6f95cba25b1a4056d6f138adcd0924e8a5736
Zohra18 <ach.zohra@gmail.com>
2020-02-12 15:10:16 +0100

    Added game struct

---
+++ RankingSystem/Model/Game.swift
@@ -0,0 +1,22 @@
+//
+//  Game.swift
+//  RankingSystem
+//
+//  Created by Zohra Achour on 12/02/2020.
+//  Copyright © 2020 Zohra Achour. All rights reserved.
+//
+
+import Foundation
+
+struct Game: Identifiable {
+    var id: UUID
+    var gameName: String
+    var gameImage: String
+    var releaseYear: Int
+    var gameGenre: [GameGenre]
+    var completionPercent: Int
+}
+
+enum GameGenre {
+    case RPG, Action, FPS, Simulation, Horror, Survival, Arcade
+}
