2bee8ccc6e4d175df9362c1b3ce17c8854381d1c
Zohra18 <ach.zohra@gmail.com>
2020-04-18 16:08:14 +0200

    updated ConnexionView design

--- RankingSystem/View/ConnexionView.swift
+++ RankingSystem/View/ConnexionView.swift
@@ -26,15 +26,33 @@ struct ConnexionView: View {
                 .padding(.top, 10)
                 .foregroundColor(Color("mainGreen"))
             
-            Text("Rank Up")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding(20)
-                .padding(.bottom, 20)
+            HStack {
+                
+                Text("Rank Up")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(20)
+                    .padding(.bottom, 20)
+                    .padding(.leading, 25)
+                
+                Image(systemName: "arrowshape.turn.up.left.fill")
+                    .rotationEffect(.degrees(90))
+                    .foregroundColor(Color("toxicGreen"))
+                    .offset(x: -33, y: -20)
+            }
+            
+            
+            
             
             // Account already existing
             VStack(spacing: 30) {
-                Text("Already have an account?")
+                VStack {
+                    Text("Already have an account?")
+                    Capsule()
+                        .frame(width: 150, height: 2, alignment: .center)
+                        .foregroundColor(Color("mainGreen"))
+                }
+                
                 
                 
                 TextField("Username", text: $username)
@@ -48,7 +66,13 @@ struct ConnexionView: View {
             Spacer()
             
             VStack(spacing: 30) {
-                Text("Sign in")
+                VStack {
+                    Text("Sign in")
+                    Capsule()
+                        .frame(width: 150, height: 2, alignment: .center)
+                        .foregroundColor(Color("mainGreen"))
+                }
+                
                 
                 
                 TextField("Username", text: $username)
@@ -60,7 +84,7 @@ struct ConnexionView: View {
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                 
                 TextField("Email", text: $email)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
             }.frame(width: 380, alignment: .center)
             
             Spacer()
@@ -69,36 +93,45 @@ struct ConnexionView: View {
                 self.showAlert.toggle()
             }, label: {
                 Text("Validate")
-                    .foregroundColor(Color("toxicGreen"))
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                    .shadow(radius: 2)
+                    .padding(10)
+                    .padding(.leading, 10)
+                    .padding(.trailing, 10)
+                    .background(Color("toxicGreen"))
+                    .cornerRadius(10)
+                
             })
+            
             
             Spacer()
             
         }.frame(width: 414)
         
-//            Testing moving keyboard on textField click
-//            .offset(y: -self.value)
-//            .animation(.spring())
-//            .onAppear {
-//
-//                NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) {
-//                    ( notification ) in
-//
-//                    let value = notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
-//                    let height = value.height
-//
-//                    self.value = height
-//                }
-//
-//                NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) {
-//                    ( notification ) in
-//
-//                    self.value = 0
-//                }
-//
-//
-//
-//        }
+        //            Testing moving keyboard on textField click
+        //            .offset(y: -self.value)
+        //            .animation(.spring())
+        //            .onAppear {
+        //
+        //                NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) {
+        //                    ( notification ) in
+        //
+        //                    let value = notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+        //                    let height = value.height
+        //
+        //                    self.value = height
+        //                }
+        //
+        //                NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) {
+        //                    ( notification ) in
+        //
+        //                    self.value = 0
+        //                }
+        //
+        //
+        //
+        //        }
         
         
         
